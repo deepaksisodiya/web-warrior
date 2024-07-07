1366de826ee82e4708357a96db37e5f566d87a52
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2024-07-07 15:44:58 +0530

    learning gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+export default function About() {
+	return <Layout>About page</Layout>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import * as React from "react"
+import * as React from "react";
+import Layout from "../components/Layout";
+import * as styles from "./../styles/home.module.css";
+import { Link } from "gatsby";
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<Layout>
+			<section className={styles.header}>
+				<div>
+					<h2>Design</h2>
+					<h3>Develop & Deploy</h3>
+					<p>UX designer & web developer based in Manchester.</p>
+					<Link className={styles.btn} to="/projects">
+						My Portfolio Projects
+					</Link>
+				</div>
+			</section>
+		</Layout>
+	);
 }
