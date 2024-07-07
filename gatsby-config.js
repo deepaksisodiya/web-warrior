74fc0e761ab0b7c0d6eabba8fef759b643b32e21
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2024-07-07 16:52:55 +0530

    learning gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,19 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+	plugins: [
+		`gatsby-transformer-remark`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `projects`,
+				path: `${__dirname}/src/projects/`,
+			},
+		},
+	],
+	siteMetadata: {
+		title: "Web Warrior",
+		description: "web dev portfolio",
+		copyright: "This website is copyright 2021 Web Warrior",
+	},
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import * as styles from "./../styles/home.module.css";
 import { Link } from "gatsby";
 
-export default function Home() {
+export default function Home({ data }) {
 	return (
 		<Layout>
 			<section className={styles.header}>
@@ -15,6 +15,7 @@ export default function Home() {
 						My Portfolio Projects
 					</Link>
 				</div>
+				<img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
 			</section>
 		</Layout>
 	);
